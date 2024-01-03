d3f7f6ebdb0a5c2636d2e20400a8ee9911762a77
Seith42 <cyril.bier42100@gmail.com>
2024-01-03 10:33:44 +0100

    Adding SpringBoot project

---
+++ apiTeams/src/test/java/com/telecom/apiTeams/ApiTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telecom.apiTeams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTeamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
