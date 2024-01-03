215b259f476cc36959df73486cd5a6e98bb18eef
Seith42 <cyril.bier42100@gmail.com>
2024-01-03 15:15:30 +0100

    Add repositories

---
+++ apiTeams/src/main/java/com/telecom/apiTeams/repository/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.telecom.apiTeams.repository;
+
+import com.telecom.apiTeams.models.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PlayerRepository extends JpaRepository<Player, Integer> {
+    List<Player> findByFirstNameIn(List<String> name);
+    List<Player> findByLastNameIn(List<String> name);
+}
---
+++ apiTeams/src/main/java/com/telecom/apiTeams/repository/TeamRepository.java
@@ -0,0 +1,10 @@
+package com.telecom.apiTeams.repository;
+
+import com.telecom.apiTeams.models.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TeamRepository extends JpaRepository<Team, Integer> {
+    List<Team> findByNameIn(List<String> names);
+}
